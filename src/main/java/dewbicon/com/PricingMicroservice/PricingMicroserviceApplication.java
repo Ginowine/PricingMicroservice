9afc1a1678c20b6b58e8f1be589a00a320a20a06
ginowine <ginowine1@gmail.com>
2020-05-12 09:43:44 +0100

    microservice can now be discovered

--- src/main/java/dewbicon/com/PricingMicroservice/PricingMicroserviceApplication.java
+++ src/main/java/dewbicon/com/PricingMicroservice/PricingMicroserviceApplication.java
@@ -2,8 +2,10 @@ package dewbicon.com.PricingMicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PricingMicroserviceApplication {
 
 	public static void main(String[] args) {
